e78ae346bcedd0023fcfae5a19c17caf65590eb8
Rafael Prado <contato@rprado.design>
2024-02-10 21:49:15 -0300

    refactor: update views

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,12 +1,14 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatController
 
 class TagCreatorView:
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatController()
+
         body = http_request.body
         product_code = body["product_code"]
 
-        print("estou na view")
-        print(product_code)
+        formatted_response = tag_creator_controller.create(product_code)
 
-        return HttpResponse(status_code=200, body={"hello": "teste"})
+        return HttpResponse(status_code=200, body=formatted_response)
